ead6b659cc1dee9573e14179448d5b366d194d8e
Elin Asplund <elin.asplund91@gmail.com>
2022-11-04 15:26:36 +0100

    minor changes img

--- src/views/NotFoundView.js
+++ src/views/NotFoundView.js
@@ -12,7 +12,7 @@ const NotFoundView = () => {
       <div><img src="https://img.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg?w=900&t=st=1666814554~exp=1666815154~hmac=060485ab25908ceab6bfc791ed8a85b24ca293037171ddfe0e68a3121b8d15c3"/></div>
       <h1>Page Not Found</h1>
     </div>
-    <div style={ { position: "fixed", bottom: "0" } }>
+    <div style={ { position: "fixed", bottom: "0", width: "100vw" } }>
       <Footer />
     </div>
   </>
